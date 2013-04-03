ee1342354f8cb3e3c14761f61262b438e70c4e4b
James Shore <jshore@jamesshore.com>
2013-04-03 15:20:04 -0700

    Factored out Selenium setup and teardown

--- src/_robot_test.js
+++ src/_robot_test.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.TXT for details.
-/*global console, require, mocha, describe, it, expect, example, beforeEach, afterEach */
+/*global console, require, mocha, describe, it, expect, example, beforeEach, afterEach, before, after */
 
 (function() {
 	"use strict";
@@ -8,13 +8,21 @@
 	var expect = require("expect.js");
 
 	describe("Google Search", function() {
+		var driver;
+
 		this.timeout(0);    // Disable Mocha's default timeout mechanism
 
-		it("should work", function(done) {
-			var driver = new webdriver.Builder().
+		before(function() {
+			driver = new webdriver.Builder().
 				withCapabilities({'browserName': 'firefox'}).
 				build();
+		});
 
+		after(function(done) {
+			driver.quit().then(done);
+		});
+
+		it("should work", function() {
 			driver.get("http://www.google.com");
 			driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
 			driver.findElement(webdriver.By.name('btnG')).click();
@@ -23,9 +31,8 @@
 					return title === 'webdriver - Google Search';
 				});
 			}, 1000);
-
-			driver.quit().then(done);
 		});
+
 	});
 
 }());
